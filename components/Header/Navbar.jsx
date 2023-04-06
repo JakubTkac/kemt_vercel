a30a21e84060125354f368183116be5a4be020b2
Jakub Tkac <jakubtkac42@gmail.com>
2023-04-07 00:00:36 +0200

    change language, CHECK Error when going to EN page from SK that doesnt exist

--- components/Header/Navbar.jsx
+++ components/Header/Navbar.jsx
@@ -2,15 +2,26 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 const Navbar = () => {
-  const { locales } = useRouter();
+  const router = useRouter();
+
+  const onChangeLanguage = (lang) => (e) => {
+    router.push(router.asPath, undefined, { locale: lang, shallow: false })
+      .reload;
+  };
+
   return (
     <>
       <h1>Change Language ...</h1>
-      {[...locales].map((locale) => (
+      {router.locales.map((locale) => (
         <Link key={locale} href="/" locale={locale}>
           {locale}
         </Link>
       ))}
+      <h1>Change Language ...</h1>
+      <div>
+        <button onClick={onChangeLanguage("sk")}>Slovak</button>
+        <button onClick={onChangeLanguage("en")}>English</button>
+      </div>
     </>
   );
 };
