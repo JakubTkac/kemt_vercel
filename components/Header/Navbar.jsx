0e82f810d1bdb5b75a74dd8e1a63e4b6f68192a5
Jakub Tkac <jakubtkac42@gmail.com>
2023-04-06 23:28:00 +0200

    buttons for change language

--- components/Header/Navbar.jsx
+++ components/Header/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
   const { locales } = useRouter();
   return (
     <>
+      <h1>Change Language ...</h1>
       {[...locales].map((locale) => (
         <Link key={locale} href="/" locale={locale}>
           {locale}
