6d21e70c73a76834e658fe30343eccb760e13416
Jakub Tkac <jakubtkac42@gmail.com>
2023-04-12 21:07:44 +0200

    fix for vercel path to public/locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     locales: ["sk", "en"],
     defaultLocale: "sk",
     localeDetection: false,
   },
+  localePath: path.resolve("./public/locales"),
 };
